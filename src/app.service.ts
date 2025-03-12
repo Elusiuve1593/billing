a9200291d769eb1868a26d799223854c0b5fdf4c
Roma <elusiuve15.09.93@icloud.com>
2025-03-12 14:42:09 +0200

    billing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { OrderCreatedEvent } from './order-created.event';
+import { ClientKafka } from '@nestjs/microservices';
+import { GetUserRequest } from './get-user-req.dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleOrderCreated({ userId, price }: OrderCreatedEvent) {
+    this.authClient
+      .send('get_user', new GetUserRequest(userId))
+      .subscribe((user) =>
+        console.log(
+          `Billing user with stripe ID ${user.stripeUserId} a price of $${price}`,
+        ),
+      );
   }
 }
